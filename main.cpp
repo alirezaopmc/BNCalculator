2e5d89384baf294f25edf8f6f070898de9666a0d
Alireza <sleepingjempa@outlook.com>
2020-09-27 01:59:17 +0330

    reorganized

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "libs/big-number/big-number.h"
-#include "libs/expression-evaluation/expression-evaluation.h"
-
-int main(int argc, char** argv) {
-    std::cout << evaluate(std::string(argv[1])) << std::endl;
-}

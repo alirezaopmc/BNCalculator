f2b8f1d9ee86e7be8c1acc871329344eff1f48e5
Alireza <sleepingjempa@outlook.com>
2020-09-28 19:59:46 +0330

    index.js with get method

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const translate = require('../libs/encryption/translate')
+const { solve } = require('./core/bncalculator')
 
 const app = express()
 
 app.get('/', function (req, res) {
-    res.json(req.query)
-    console.log(req.query)
+    let expression = translate(req.query.exp)
+    let ans = solve(expression)
+    res.json(ans)
 })
 
 app.listen(3000)

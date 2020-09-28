f0b755680375e56296282914933fc3db84956ab9
Alireza <sleepingjempa@outlook.com>
2020-09-28 19:40:41 +0330

    bncalculator.js fixed

--- backend/core/bncalculator.js
+++ backend/core/bncalculator.js
@@ -1,35 +1,19 @@
-const { exec } = require("child_process")
+const { execSync } = require("child_process")
 const fs = require("fs")
 
 const solve = (str) => {
     let command = `${__dirname}/./a.out '${str}'`
     console.log(command)
-    const out = exec(command, (error, stdout, stderr) => {
-        console.log(`stdout: ${stdout}`)
-        let out = {
-            value: stdout
-        }
-        save(out)
-    })
-    return out
-}
-
-const save = (out) => {
-    let outString = JSON.stringify(out).replace('\\n', '')
-    fs.writeFileSync(__dirname + '/out.json', outString)
-}
-
-const load = () => {
-    let inString
-    let path = `${__dirname}/out.json`
-    return require(path)
+    const result = execSync(command).toString().replace('\n', '')
+    console.log(`result = ${result}`)
+    return result
 }
 
 module.exports = {
     solve,
-    save,
-    load
+    // save,
+    // load
 }
 
-// solve('13*2')
+// console.log(solve('13*2'))
 // console.log(load())

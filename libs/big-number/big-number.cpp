22f573cfc62e3240c3ec8eef34466cf3e3d3a9b5
Alireza <sleepingjempa@outlook.com>
2020-10-01 19:31:34 +0330

    BigNumber Division Fixed

--- libs/big-number/big-number.cpp
+++ libs/big-number/big-number.cpp
@@ -207,6 +207,11 @@ BigNumber BigNumber::subtractString(const std::string &other) {
 }
 
 BigNumber BigNumber::multiply(BigNumber other) {
+    if (other == 10) {
+        std::string result = (*this)._numberString + "0";
+        return BigNumber(result);
+    }
+
     BigNumber b1 = other > *this ? other : *this;
     BigNumber b2 = other > *this ? *this : other;
     if (b1.isNegative() || b2.isNegative()) {
@@ -279,6 +284,17 @@ BigNumber BigNumber::divide(BigNumber other) {
     if (other == 0) {
         std::cerr << "You cannot divide by 0!" << std::endl;
     }
+
+    if (other == 1) {
+        return *this;
+    }
+
+    if (other == 10) {
+        std::string result = (*this)._numberString;
+        result.pop_back();
+        return BigNumber(result);
+    }
+
     BigNumber b1 = *this, b2 = other;
     bool sign = false;
     if (b1.isNegative() && b2.isNegative()) {
@@ -293,12 +309,44 @@ BigNumber BigNumber::divide(BigNumber other) {
         b2.negate();
         sign = true;
     }
-    BigNumber quotient = 0;
-    while (b1 >= b2) {
-        b1 -= b2;
-        ++quotient;
+    
+    BigNumber quotient("0");
+    int n = b1._numberString.length();
+    int m = b2._numberString.length();
+
+    if (n < m) {
+        return 0;
+    } else if (n-m < 5) {
+        while (b1 >= b2 && b1 > 0) {
+            b1 -= b2;
+            quotient += 1;
+        }
+    } else {
+        BigNumber a = b1;
+        BigNumber b = b2;
+
+        while (b * 10 < a) b *= 10;
+
+        while (a >= b && b > 0) {
+            BigNumber temp = a;
+            a -= b;
+            quotient += 1;
+            if (a < b && a > 0) {
+                b /= 10;
+                quotient *= 10;
+            }
+            if (a == 0) {
+                while (temp._numberString[temp._numberString.length() - 1] == '0') {
+                    temp /= 10;
+                    quotient *= 10;
+                }
+            }
+        }
     }
-    if (sign) quotient.negate();
+
+    if (sign) {
+        quotient.negate();
+    }
     return quotient;
 }
 
